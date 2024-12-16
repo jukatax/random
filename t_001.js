ff6f6a9633078acb631a7384ecd77a2bc7eed4be
Yuliyan Yordanov <julybeat31@hotmail.com>
2024-12-16 17:15:26 +0000

    Create t_001.js

---
+++ t_001.js
@@ -0,0 +1,49 @@
+
+// #region for questions please contact myself and cc your recruiter
+  // mailto:yuliyan.yordanov@tesco.com
+// #endregion
+
+// #region <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Task 1 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+/**
+ * 1.Filter the following array for those born between 1400 & 1600
+ * 2.Sort the whole array by last name
+ * 3.Create a new array of objects with only 2 key-value pairs: name & age
+ */
+const inventors = [
+    { "first": 'Albert', "last": 'Einstein', "year": 1879, "passed": 1955 },
+    { "first": 'Isaac', "last": 'Newton', "year": 1643, "passed": 1727 },
+    { "first": 'Galileo', "last": 'Galilei', "year": 1564, "passed": 1642 },
+    { "first": 'Marie', "last": 'Curie', "year": 1867, "passed": 1934 },
+    { "first": 'Johannes', "last": 'Kepler', "year": 1571, "passed": 1630 },
+    { "first": 'Nicolaus', "last": 'Copernicus', "year": 1473, "passed": 1543 },
+    { "first": 'Max', "last": 'Planck', "year": 1858, "passed": 1947 }
+  ];
+  //Ans:1 Filter here
+
+
+  //Ans:2 Sort here
+
+
+  //Ans:3 new array here
+
+
+  //#endregion
+
+  // #region <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Task 2 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  /**
+* 1.Create and array of 100 random numbers (integers)
+* 2.Sort the array
+* 3.Write a function that will accept two parameters - an array of sorted numbers and a number to look for. The function should return the number if found or return 0 if the number doesn't exist. The algorithm should most efficient as in teh same that was asked during the interview and perform at most 6 checks for 100 entries.
+*/
+let  arr100rnd = [],
+  findNumber = function(){
+    //Ans:3  your algorithm here
+
+  };
+//Ans:1 Generate the array of 100 random numbers between 1-1000
+
+
+//Ans:2 Sort the array
+
+
+//#endregion
